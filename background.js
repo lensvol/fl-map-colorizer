3dc77a26636d25809e26304ebea954f3fcabad36
Kirill Borisov <lensvol@gmail.com>
2023-01-14 23:25:50 +0100

    Add support for Manifest v3 via declarativeNetRequest

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if (details.url.indexOf("https://s3.us-east-1.amazonaws.com/maps-production.fallenlondon.com/6.5.33/london/fallback/london-fallback.jpg") !== -1) {
-            return {redirectUrl: "https://i.redd.it/jvp1ojvicpk41.png"};
-        }
-    },
-    {
-        urls: ["https://*.amazonaws.com"],
-    },
-    ["blocking"]
-);
-
+if (!chrome.declarativeNetRequest) {
+    chrome.webRequest.onBeforeRequest.addListener(
+        function (details) {
+            if (details.url.indexOf("https://s3.us-east-1.amazonaws.com/maps-production.fallenlondon.com/6.5.33/london/fallback/london-fallback.jpg") !== -1) {
+                return {redirectUrl: "https://i.redd.it/jvp1ojvicpk41.png"};
+            }
+        },
+        {
+            urls: ["https://*.amazonaws.com"],
+        },
+        ["blocking"]
+    );
+}
