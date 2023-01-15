57f751f2484ed72ae07c32a4a2041254dc0c7e77
Kirill Borisov <lensvol@gmail.com>
2023-01-15 15:41:57 +0100

    Make webRequest-based redirector more robust.

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 if (!chrome.declarativeNetRequest) {
     chrome.webRequest.onBeforeRequest.addListener(
         function (details) {
-            if (details.url.indexOf("https://s3.us-east-1.amazonaws.com/maps-production.fallenlondon.com/6.5.33/london/fallback/london-fallback.jpg") !== -1) {
+            if (details.url.indexOf("london-fallback.jpg") !== -1) {
                 return {redirectUrl: "https://i.redd.it/jvp1ojvicpk41.png"};
             }
         },
         {
-            urls: ["https://*.amazonaws.com"],
+            urls: ["https://*.amazonaws.com/*"],
         },
         ["blocking"]
     );
